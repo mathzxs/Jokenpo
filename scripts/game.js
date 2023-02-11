ef6f84fa1f9aae41daead3af42666f7404d8d9b8
Matheus Gabriel <71954064+mathzxs@users.noreply.github.com>
2023-02-11 16:41:10 +0000

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -8,11 +8,11 @@ const jokenpo = () => {
     let ties = 0;
     const options = ['scissors', 'rock', 'paper'];
 
-    for (let i = 0; i < rounds; i++) {
+    const playRound = () => {
       console.log(`Round ${i + 1}:`);
-      let player = prompt('Rock, paper, or scissors?').toLowerCase();
+      let player = prompt('Rock, paper, or scissors?').toLowerCase().trim();
       while (!options.includes(player)) {
-        player = prompt('Invalid choice. Rock, paper, or scissors?').toLowerCase();
+        player = prompt('Invalid choice. Rock, paper, or scissors?').toLowerCase().trim();
       }
       const computer = options[Math.floor(Math.random() * options.length)];
       console.log(`You played ${player} and the computer played ${computer}.`);
@@ -45,14 +45,14 @@ const jokenpo = () => {
         }
       }
       console.log(`Score: Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`);
+    };
+
+    for (let i = 0; i < rounds; i++) {
+      playRound();
     }
     console.log(`Final Results: Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`);
     playAgain = window.confirm('Do you want to play again?');
-    if (playAgain===true){
-    console.clear()
-   };
   }
 };
 
 jokenpo();
-
